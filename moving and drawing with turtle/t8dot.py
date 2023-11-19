465170787164f157b64c995ff9a25c7a67778cf3
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2023-11-20 00:28:45 +0500

    t8dot.py created and updated

---
+++ moving and drawing with turtle/t8dot.py
@@ -0,0 +1,36 @@
+# # import package 
+# import turtle 
+  
+  
+# # motion 
+# turtle.forward(100) 
+  
+# # dot with 
+# # 60 diameter 
+# # yellow color 
+# turtle.dot(60, color="yellow") 
+
+
+# import package 
+import turtle 
+  
+  
+# delay the turtle work speed 
+# for better understandings 
+turtle.delay(500) 
+  
+# hide the turtle 
+turtle.ht() 
+  
+# some dots with diameter and color 
+turtle.dot(size = 200, color="red") 
+turtle.dot(size=180, color="orange") 
+turtle.dot(size=160, color="yellow") 
+turtle.dot(size=140, color="green") 
+turtle.dot(size=120, color="blue") 
+turtle.dot(size=100, color="indigo") 
+turtle.dot(size=80, color="violet") 
+turtle.dot(size=60, color="white") 
+  # write text 
+turtle.write("GFG", align="center", 
+             font=('Verdana', 12, 'bold'))
