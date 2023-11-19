a643722e508a9be0fc66e521eaad5d0eba5a5d0b
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2023-11-19 22:38:18 +0500

    file created and code also written

---
+++ moving and drawing with turtle/t2_backward.py
@@ -0,0 +1,19 @@
+# importing package 
+import turtle 
+  
+  
+# move the turtle backward by 50 
+turtle.backward(50) 
+  
+# change the direction 
+turtle.right(90) 
+  
+# move the turtle backward by 50 again 
+turtle.backward(50)
+
+# change the direction 
+turtle.left(150) 
+  
+# move the turtle backward by 50 again 
+turtle.backward(50)
+
