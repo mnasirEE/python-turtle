edc8f1719fcafe152eaa29624499fe010819fa38
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2023-11-19 22:34:27 +0500

    Code is written

---
+++ moving and drawing with turtle/t1_forward.py
@@ -0,0 +1,17 @@
+import turtle 
+  
+# moves the pen in the  
+# forward direction by  
+# 110 pixels 
+turtle.forward(110) 
+  
+# changes the direction of  
+# the pen by 10 degrees in the 
+# left direction 
+turtle.right(90) 
+  
+# moves the pen in the  
+# forward direction in  
+# the new direction by 
+# 110 pixels 
+turtle.forward(110)
