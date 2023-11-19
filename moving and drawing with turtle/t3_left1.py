e8a5cb9172333bb984e50268569598a3563c3a38
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2023-11-19 22:51:47 +0500

    code written

---
+++ moving and drawing with turtle/t3_left1.py
@@ -0,0 +1,37 @@
+# # Example 1
+# # importing package 
+# import turtle 
+  
+  
+# # move the turtle forward by  
+# # 100 unit distance in the 
+# # direction of head of turtle 
+# turtle.forward(100) 
+  
+# # change the direction of turtle 
+# # by 90 degrees to the left. 
+# turtle.left(90) 
+  
+# # move the turtle forward by  
+# # 100 unit distance in the  
+# # direction of head of turtle 
+# turtle.forward(100)
+
+# Example2
+
+# importing package 
+import turtle 
+  
+# Loop for pattern 
+for i in range(10): 
+    
+  # move the turtle forward by  
+  # 100+variable unit distance 
+  # in the direction of head of 
+  # turtle 
+  turtle.forward(100+10*i) 
+#   print(i)   
+  # change the direction of turtle 
+  # by 90 degrees to the left. 
+  turtle.left(90)
+#   turtle.forward(100+10*i) 
