fe8ba598d3f25394ea1f42ea2008707524b83074
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2023-11-20 00:30:38 +0500

    t9home.py created and updated

---
+++ moving and drawing with turtle/t9home.py
@@ -0,0 +1,32 @@
+
+# import package 
+import turtle 
+  
+  
+# check turtle position 
+print(turtle.position()) 
+  
+# motion 
+turtle.forward(100) 
+  
+# check turtle position 
+print(turtle.position()) 
+  
+# set turtle to home 
+turtle.home() 
+  
+# check turtle position 
+print(turtle.position()) 
+  
+# motion 
+turtle.right(90) 
+turtle.forward(100) 
+  
+# check turtle position 
+print(turtle.position()) 
+  
+# set turtle to home 
+turtle.home() 
+  
+# check turtle position 
+print(turtle.position())
